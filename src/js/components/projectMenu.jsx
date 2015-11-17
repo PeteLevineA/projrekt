2cd537c3d874a8f69b5e3842a0e9e65e218bd2f4
PeteLevineA <pete@ionisix.com>
2015-11-16 23:52:42 -0500

    stubbed project menu

--- src/js/components/projectMenu.jsx
+++ src/js/components/projectMenu.jsx
@@ -4,10 +4,16 @@ var React = require('react');
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var ProjectMenu = React.createClass({
+	propTypes: {
+		project: React.PropTypes.object
+	},
+	handleHover: function() {
+		
+	},
 	render: function() {
 		return <ReactCSSTransitionGroup transitionName="projectMenu" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-        	<div key="projectMenu">
-				
+        	<div key="projectMenu" onHover={this.handleHover}>
+				<span className="title">{this.props.project.name}</span>
 			</div>
         </ReactCSSTransitionGroup>;
 	}
